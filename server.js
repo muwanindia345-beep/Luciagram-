107f16ae1bd21b1112ef06ad98041b68eb5a02b6
Prime <your@email.com>
2026-05-21 14:26:19 +0530

    fix: increase payload limit to 50mb

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ const httpServer = http.createServer(app);
 app.use(helmet());
 app.use(cors());
 app.use(morgan('dev'));
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/users');
