9b306159acfaee3696e76393fa00f00254ed8f02
Prime <your@email.com>
2026-05-30 00:39:30 +0530

    feat: Capacitor Android + GitHub Actions setup

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.luciaorg.luciagram',
+  appName: 'LuciaGram',
+  webDir: 'build'
+};
+
+export default config;
